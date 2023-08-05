d93f1d6c47ca81186c443874f2282641061c6f27
sps@#254 <suryapratap2542@gmail.com>
2023-08-05 23:38:05 +0530

    main

--- main.js
+++ main.js
@@ -1,44 +1,62 @@
 function GetInfo() {
-    var newName = document.getElementById("cityInput");
-    var cityName = document.getElementById("cityName");
-    cityName.innerHTML = "--"+newName.value+"--";
-fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=32ba0bfed592484379e51106cef3f204')
-.then(response => response.json())
-.then(data => {
-    //to find the the min and max values for each day
-    for(i = 0; i<5; i++){
-        document.getElementById("day" + (i+1) + "Min").innerHTML = "Min: " + Number(data.list[i].main.temp_min - 273.15).toFixed(1)+ "°";        
-    }
-    for(i = 0; i<5; i++){
-        document.getElementById("day" + (i+1) + "Max").innerHTML = "Max: " + Number(data.list[i].main.temp_max - 273.15).toFixed(2) + "°";
-    }
-    //to find Weather Icons
-     for(i = 0; i<5; i++){
-        document.getElementById("img" + (i+1)).src = "http://openweathermap.org/img/wn/"+
-        data.list[i].weather[0].icon
-        +".png";
-    }    
-    console.log(data)
-})
-.catch(err => alert("Please enter the correct location"))
-}
-function DefaultScreen(){
-    document.getElementById("cityInput").defaultValue = "Delhi";
+    var newName = document.getElementById('cityInput');
+    var cityName = document.getElementById('cityName');
+    cityName.innerHTML = '--' + newName.value + '--';
+    fetch(
+      'https://api.openweathermap.org/data/2.5/forecast?q=' +
+        newName.value +
+        '&appid=32ba0bfed592484379e51106cef3f204'
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        //to find the the min and max values for each day
+        for (i = 0; i < 5; i++) {
+          document.getElementById('day' + (i + 1) + 'Min').innerHTML =
+            'Min: ' +
+            Number(data.list[i].main.temp_min - 273.15).toFixed(1) +
+            '°';
+        }
+  
+        for (i = 0; i < 5; i++) {
+          document.getElementById('day' + (i + 1) + 'Max').innerHTML =
+            'Max: ' +
+            Number(data.list[i].main.temp_max - 273.15).toFixed(2) +
+            '°';
+        }
+        //to find Weather Icons
+        for (i = 0; i < 5; i++) {
+          document.getElementById('img' + (i + 1)).src =
+            'http://openweathermap.org/img/wn/' +
+            data.list[i].weather[0].icon +
+            '.png';
+        }
+        console.log(data);
+      })
+      .catch((err) => alert('Please enter the correct location'));
+  }
+  function DefaultScreen() {
+    document.getElementById('cityInput').defaultValue = 'Delhi';
     GetInfo();
-}
-//to find and displaying the text for the upcoming five days of the week
-var d = new Date();
-var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",];
-//Function to get the correct integer for the index of the days array
-function CheckDay(day){
-    if(day + d.getDay() > 6){
-        return day + d.getDay() - 7;
+  }
+  //to find and displaying the text for the upcoming five days of the week
+  var d = new Date();
+  var weekday = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  //Function to get the correct integer for the index of the days array
+  function CheckDay(day) {
+    if (day + d.getDay() > 6) {
+      return day + d.getDay() - 7;
+    } else {
+      return day + d.getDay();
     }
-    else{
-        return day + d.getDay();
-    }
-}
-    for(i = 0; i<5; i++){
-        document.getElementById("day" + (i+1)).innerHTML = weekday[CheckDay(i)];
-    }
-   
+  }
+  for (i = 0; i < 5; i++) {
+    document.getElementById('day' + (i + 1)).innerHTML = weekday[CheckDay(i)];
+  }
